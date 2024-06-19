8655da9d3f5232dc02b776b2c572e4611cbc5264
OtakuIsMe <duynmse173649@fpt.edu.vn>
2024-06-19 21:13:52 +0700

    update course learning

--- FE/src/components/Courses/CoursesLearningMap/CoursesLearningMap.jsx
+++ FE/src/components/Courses/CoursesLearningMap/CoursesLearningMap.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 import './CoursesLearningMap.css';
 
+import CurriculumCourse from "../../Items/CurriculumCourse/CurriculumCourse";
+
 export default function CoursesLearningMap() {
+    const datas = [
+        {
+
+        },
+        {
+
+        },
+        {
+
+        },
+        {
+
+        },
+        {
+
+        },
+    ]
+
     return (
         <div id="courses-learning-map">
             <p className="title">Course Content</p>
             <div className="course-content-container">
+                {datas.map((data, index) => {
+                    return (
+                        <React.Fragment>
+                            <div className="chapter-detail" key={index}>
 
+                            </div>
+                        </React.Fragment>
+                    )
+                })
+
+                }
             </div>
         </div>
     )
