6d005d63cd7dbfceb7b87bb91ac964f868aacfc6
OtakuIsMe <duynmse173649@fpt.edu.vn>
2024-06-24 13:22:42 +0700

    create bar switch

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Home from './pages/Home/Home'
 import CourseDetailPage from './pages/UserPage/CourseDetailPage/CourseDetailPage';
 import LoginPage from './pages/AuthPage/LoginPage/LoginPage';
 import RegisterPage from './pages/AuthPage/RegisterPage/RegisterPage';
 import StudentProfilePage from './pages/StudentPage/ProfilePage/StudentProfilePage';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import CourseLearning from './pages/StudentPage/CourseLearning/CourseLearning';
+import InstructorProfilePage from './pages/InstructorPage/ProfilePage/InstructorProfilePage';
 
 function App() {
   return (
@@ -20,10 +22,13 @@ function App() {
         </Route>
         <Route path='/courses'>
           <Route path=':courseName' element={<CourseDetailPage />} />
-          <Route path='learning/:courseName' element={<CourseLearning/>}/>
+          <Route path='learning/:courseName' element={<CourseLearning />} />
         </Route>
         <Route path='/student'>
           <Route path='profile' element={<StudentProfilePage />} />
+        </Route>
+        <Route path='/instructor'>
+          <Route path='profile' element={<InstructorProfilePage />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ FE/src/components/Instructor/Profile/CreatedCourse/CreatedCourse.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './CreatedCourse.css'
+
+export default function CreatedCourse() {
+    return (
+        <div id="created-course">
+
+        </div>
+    )
+}
