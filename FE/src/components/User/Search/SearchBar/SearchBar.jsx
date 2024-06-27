9e0c8aa031152543d37cf18048bf019c54f9169e
OtakuIsMe <duynmse173649@fpt.edu.vn>
2024-06-27 15:48:26 +0700

    pull

--- FE/src/components/User/Search/SearchBar/SearchBar.jsx
+++ FE/src/components/User/Search/SearchBar/SearchBar.jsx
@@ -1,11 +1,30 @@
 import React, { useEffect, useState } from "react";
 import './SearchBar.css';
 
+import { IoIosSearch } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+
 export default function SearchBar({ query }) {
     const [valueInput, setValueInput] = useState(query)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        setValueInput(query);
+    }, [query]);
+
+    const handleInputChange = (e) => {
+        setValueInput(e.target.value);
+        navigate(`/search?q=${e.target.value}`);
+    };
+
     return (
         <div id="search-bar">
-
+            <div className="search-icon">
+                <IoIosSearch />
+            </div>
+            <div className="input-container">
+                <input type="text" onChange={handleInputChange} value={valueInput} />
+            </div>
         </div>
     )
 }
