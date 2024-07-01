ee5363db1013ddfcb43b4e9364b6e72adc4e6a39
NET_10_NguyenHuuCanh <canhnhce171635@fpt.edu.vn>
2024-07-01 10:22:49 +0700

    update

--- FE/src/components/Admin/ManageCourse/ManageCourse.jsx
+++ FE/src/components/Admin/ManageCourse/ManageCourse.jsx
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import './ManageCourse.css';
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
-
-export default function ManageCourse(){
-    return(
-        <div id="ManageCourse">
-            
-
-        
-        </div>
-    )
-}
+export default function ManageCourse() {
+    return (
+        <Pagination>
+            <PaginationItem>
+                <PaginationLink
+                    first
+                    href="#"
+                />
+            </PaginationItem>
+            <PaginationItem>
+                <PaginationLink
+                    href="#"
+                    previous
+                />
+            </PaginationItem>
+            <PaginationItem>
+                <PaginationLink href="#">
+                    1
+                </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+                <PaginationLink href="#">
+                    2
+                </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+                <PaginationLink href="#">
+                    3
+                </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+                <PaginationLink href="#">
+                    4
+                </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+                <PaginationLink href="#">
+                    5
+                </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+                <PaginationLink
+                    href="#"
+                    next
+                />
+            </PaginationItem>
+            <PaginationItem>
+                <PaginationLink
+                    href="#"
+                    last
+                />
+            </PaginationItem>
+        </Pagination>
+    );
+};
