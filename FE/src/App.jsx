64f6b3e6cf54e9058dabf66ef7766ba398cfdbff
@NET_10_NguyenChiHai <HaiNCCE171674@fpt.edu.vn>
2024-07-06 23:50:06 +0630

    update

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -24,34 +24,51 @@ import RequestsPage from './pages/Admin/RequestsPage/RequestsPage';
 import SettingPage from './pages/Admin/SettingPage/SettingPage';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Home />} />
-          <Route path='login' element={<LoginPage />} />
-          <Route path='signup' element={<RegisterPage />} />
-          <Route path='payment' element={<PaymentPage />} />
-          <Route path='search' element={<SearchPage />} />
-        </Route>
-        <Route path='/courses'>
-          <Route path=':courseName' element={<CourseDetailPage />} />
-          <Route path='learning/:courseType/:itemName' element={<CourseLearning />} />
-        </Route>
-        <Route path='/student'>
-          <Route path='profile' element={<StudentProfilePage />} />
-          <Route path='my-learning'>
-            <Route path='completed' element={<CourseCompletedPage />} />
-            <Route path='following' element={<FollowingPage />} />
-            <Route path='in-progress' element={<CourseInProgressPage />} />
-            <Route path='saved' element={<CourseSavedPage />} />
-          </Route>
-        </Route>
-        <Route path='/instructor'>
-          <Route path='profile' element={<InstructorProfilePage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+     return (
+          <BrowserRouter>
+               <Routes>
+                    <Route path="/">
+                         <Route index element={<Home />} />
+                         <Route path='login' element={<LoginPage />} />
+                         <Route path='signup' element={<RegisterPage />} />
+                         <Route path='payment' element={<PaymentPage />} />
+                         <Route path='search' element={<SearchPage />} />
+                    </Route>
+                    <Route path='/courses'>
+                         <Route path=':courseName' element={<CourseDetailPage />} />
+                         <Route path='learning/:courseType/:itemName' element={<CourseLearning />} />
+                    </Route>
+                    <Route path='/student'>
+                         <Route path='profile' element={<StudentProfilePage />} />
+                         <Route path='my-learning'>
+                              <Route path='completed' element={<CourseCompletedPage />} />
+                              <Route path='following' element={<FollowingPage />} />
+                              <Route path='in-progress' element={<CourseInProgressPage />} />
+                              <Route path='saved' element={<CourseSavedPage />} />
+                         </Route>
+                    </Route>
+                    <Route path='/instructor'>
+                         <Route path='profile' element={<InstructorProfilePage />} />
+                    </Route>
+<<<<<<< HEAD
+
+     <Route path='/admin'>
+          <Route path='dashboard' element={<DashBoard />} />
+          <Route path='instructor' element={<ManageInstructorPage />} />
+          <Route path='student' element={<ManageStudentPage />} />
+          <Route path='course' element={<ManageCoursePage />} />
+          <Route path='requests' element={<RequestsPage />} />
+          <Route path='setting' element={<SettingPage />} />
+
+
+
+
+
+     </Route>
+=======
+>>>>>>> b5d8e5c4a5ebd17a87f588dc3158745baddd2c14
+      </Routes >
+    </BrowserRouter >
   )
 }
 
