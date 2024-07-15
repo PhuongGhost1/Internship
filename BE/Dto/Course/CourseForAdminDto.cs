88807e332782074d1dd537c0070f2fa0ae0b170e
PhuongGhost1 <trngphng94@gmail.com>
2024-07-15 22:14:09 +0700

    Write and fetch on certification page for student page

--- BE/Dto/Course/CourseForAdminDto.cs
+++ BE/Dto/Course/CourseForAdminDto.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BE.Dto.CategoryCourse;
+using BE.Dto.Chapter;
+using BE.Dto.Course.Chapter;
 using BE.Dto.ImageD;
 using BE.Dto.User;
 using BE.Models;
@@ -19,6 +21,7 @@ namespace BE.Dto.Course
         public bool? IsVisible {get; set;}
         public List<ImageForAdminDto> Images { get; set; } = new List<ImageForAdminDto>();
           public List<CategoryCourseDto> CateCoruse = new List<CategoryCourseDto>();
+          public List<ChapterItemDto> Chapters = new List<ChapterItemDto>();
          public UserInfoManageByAdminDto? User {get; set;}
         public DateTime? CreatedAt {get; set;}
         public DateTime? UpdatedAt {get; set;}
